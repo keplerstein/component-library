c7ecf9e2e55cf9f2f51a72a21b775446b141448d
Nicholas Olivier <antimated@gmail.com>
2019-07-17 11:22:03 +0200

    Remove sample button

--- sample-button/index.js
+++
@@ -1,9 +0,0 @@
-export default function() {
-    console.log("initializing button");
-
-    document.querySelectorAll(".sample-button").forEach(function(tmp) {
-        tmp.addEventListener("click", function() {
-            alert("Button clicked");
-        });
-    });
-}
